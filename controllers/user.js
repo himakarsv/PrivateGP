cf864002805636f57b1d50877325c19a47e72552
“[firstname <himakarhimmu@gmail.com>
2024-09-04 19:41:29 +0530

    college controller return change with try catch

--- controllers/user.js
+++ controllers/user.js
@@ -2,84 +2,88 @@ const { PrismaClient } = require("@prisma/client");
 const prisma = new PrismaClient();
 
 const editProfile = async (req, res) => {
-  if (req.role === "INDIVIDUAL") {
-    const {
-      taxExemptionRequired,
-      anonymous,
-      pan,
-      salutation,
-      name,
-      residency,
-    } = req.body;
-    const email = req.userEmail;
-    const user = await prisma.user.findUnique({
-      where: { email },
-    });
-
-    if (!user) {
-      return res.status(404).json({ message: "User not found" });
-    }
-
-    // Update the User model
-    const updatedUser = await prisma.user.update({
-      where: { email },
-      data: {
-        taxExemptionRequired: Boolean(taxExemptionRequired),
-        anonymous: Boolean(anonymous),
-      },
-    });
-
-    await prisma.individual.update({
-      where: { email },
-      data: {
+  try {
+    if (req.role === "INDIVIDUAL") {
+      const {
+        taxExemptionRequired,
+        anonymous,
         pan,
         salutation,
         name,
         residency,
-      },
-    });
-    return res.json({ message: "User updated successfully" });
-  }
+      } = req.body;
+      const email = req.userEmail;
+      const user = await prisma.user.findUnique({
+        where: { email },
+      });
 
-  if (req.role === "COMPANY") {
-    const {
-      taxExemptionRequired,
-      anonymous,
-      companyID,
-      pan,
-      salutation,
-      name,
-      contactPersonName,
-    } = req.body;
-    const email = req.userEmail;
-    const user = await prisma.user.findUnique({
-      where: { email },
-    });
+      if (!user) {
+        return res.status(404).json({ message: "User not found" });
+      }
 
-    if (!user) {
-      return res.status(404).json({ message: "User not found" });
+      // Update the User model
+      const updatedUser = await prisma.user.update({
+        where: { email },
+        data: {
+          taxExemptionRequired: Boolean(taxExemptionRequired),
+          anonymous: Boolean(anonymous),
+        },
+      });
+
+      await prisma.individual.update({
+        where: { email },
+        data: {
+          pan,
+          salutation,
+          name,
+          residency,
+        },
+      });
+      return res.json({ message: "User updated successfully" });
     }
 
-    // Update the User model
-    const updatedUser = await prisma.user.update({
-      where: { email },
-      data: {
-        taxExemptionRequired: Boolean(taxExemptionRequired),
-        anonymous: Boolean(anonymous),
-      },
-    });
-
-    await prisma.company.update({
-      where: { email },
-      data: {
+    if (req.role === "COMPANY") {
+      const {
+        taxExemptionRequired,
+        anonymous,
         companyID,
         pan,
         salutation,
         name,
         contactPersonName,
-      },
-    });
-    return res.json({ message: "User updated successfully" });
+      } = req.body;
+      const email = req.userEmail;
+      const user = await prisma.user.findUnique({
+        where: { email },
+      });
+
+      if (!user) {
+        return res.status(404).json({ message: "User not found" });
+      }
+
+      // Update the User model
+      const updatedUser = await prisma.user.update({
+        where: { email },
+        data: {
+          taxExemptionRequired: Boolean(taxExemptionRequired),
+          anonymous: Boolean(anonymous),
+        },
+      });
+
+      await prisma.company.update({
+        where: { email },
+        data: {
+          companyID,
+          pan,
+          salutation,
+          name,
+          contactPersonName,
+        },
+      });
+      return res.json({ message: "User updated successfully" });
+    }
+  } catch (error) {
+    return res.json({ error, message: "An error occured" });
   }
 };
 
